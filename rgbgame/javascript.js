d6fa21652c7e2037bd05a6f6f6c200b2aff82581
AUSTIN\dphipps <david.phipps@q2ebanking.com>
2017-07-12 16:05:01 -0500

    4th commit

--- rgbgame/javascript.js
+++ rgbgame/javascript.js
@@ -1,11 +1,62 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay")
 var messageDisplay = document.querySelector("#message")
 colorDisplay.textContent = pickedColor;
-var h1 = document.querySelector("h1")
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i =0; i<squares.length; i++){
+		if(colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+
+});
+
+hardBtn.addEventListener("click", function(){
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i =0; i<squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "block";
+	}
+
+});
+
+
+
+resetButton.addEventListener("click", function(){
+	//generate all new colors
+	colors = generateRandomColors(numSquares);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked colors
+	//change colors of squares on page
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i<squares.length; i++){
+		//add initial colors to squares
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "steelblue";
+})
 
 for(var i = 0; i<squares.length; i++){
 	//add initial colors to squares
@@ -17,6 +68,7 @@ for(var i = 0; i<squares.length; i++){
 		//compare color to picked color
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again";
 			changeColors(pickedColor);
 			h1.style.backgroundColor = clickedColor;
 		} else {
