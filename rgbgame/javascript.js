d9566b6f1bafe1b90c64d62f4ba9584442b1d901
David Phipps <morningsides@hotmail.com>
2017-07-13 18:30:20 -0500

    done

--- rgbgame/javascript.js
+++ rgbgame/javascript.js
@@ -1,15 +1,18 @@
 var numSquares = 6;
-var colors = generateRandomColors(numSquares);
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay")
 var messageDisplay = document.querySelector("#message")
-colorDisplay.textContent = pickedColor;
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode")
 
-for(var i =0; i < modeButtons.length; i++){
+init();
+
+function init() {
+	//mode buttons event listeners
+	for(var i =0; i < modeButtons.length; i++){
 	modeButtons[i].addEventListener("click",function(){
 		modeButtons[0].classList.remove("selected");
 		modeButtons[1].classList.remove("selected");
@@ -17,8 +20,35 @@ for(var i =0; i < modeButtons.length; i++){
 		//if loop using ternary operator format
 		this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
 		reset();
-	});
+		});
+	}
+
+	resetButton.addEventListener("click", function(){
+	reset();
+	})
+
+	for(var i = 0; i<squares.length; i++){
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function(){
+		//grab color of clicked square
+		var clickedColor = this.style.backgroundColor;
+		//compare color to picked color
+		if(clickedColor === pickedColor){
+			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again";
+			changeColors(pickedColor);
+			h1.style.backgroundColor = clickedColor;
+		} else {
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
+			}		
+		});
+	}
+
+	reset();
 }
+
 
 function reset(){
 	colors = generateRandomColors(numSquares);
@@ -41,62 +71,8 @@ function reset(){
 }
 
 
-// easyBtn.addEventListener("click", function(){
-// 	hardBtn.classList.remove("selected");
-// 	easyBtn.classList.add("selected");
-// 	numSquares = 3;
-// 	colors = generateRandomColors(numSquares);
-// 	pickedColor = pickColor();
-// 	colorDisplay.textContent = pickedColor;
-// 	for(var i =0; i<squares.length; i++){
-// 		if(colors[i]){
-// 			squares[i].style.backgroundColor = colors[i];
-// 		} else {
-// 			squares[i].style.display = "none";
-// 		}
-// 	}
-
-// });
-
-// hardBtn.addEventListener("click", function(){
-// 	hardBtn.classList.add("selected");
-// 	easyBtn.classList.remove("selected");
-// 	numSquares = 6;
-// 	colors = generateRandomColors(numSquares);
-// 	pickedColor = pickColor();
-// 	colorDisplay.textContent = pickedColor;
-// 	for(var i =0; i<squares.length; i++){
-// 		squares[i].style.backgroundColor = colors[i];
-// 		squares[i].style.display = "block";
-// 	}
-
-// });
 
 
-
-resetButton.addEventListener("click", function(){
-	reset();
-})
-
-for(var i = 0; i<squares.length; i++){
-	//add initial colors to squares
-	squares[i].style.backgroundColor = colors[i];
-	//add click listeners to squares
-	squares[i].addEventListener("click", function(){
-		//grab color of clicked square
-		var clickedColor = this.style.backgroundColor;
-		//compare color to picked color
-		if(clickedColor === pickedColor){
-			messageDisplay.textContent = "Correct!";
-			resetButton.textContent = "Play Again";
-			changeColors(pickedColor);
-			h1.style.backgroundColor = clickedColor;
-		} else {
-			this.style.backgroundColor = "#232323";
-			messageDisplay.textContent = "Try Again";
-		}		
-	});
-}
 
 function changeColors(color){
 	//loop through all squares
--- todosProject/assets/js/todos.js
+++ todosProject/assets/js/todos.js
@@ -1,14 +1,28 @@
 // Check off Specific Todos by Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
-
 });
 
 //Click on X to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   event.stopPropagation();
-})
+});
 
+
+$("input[type='text']").keypress(function(event){
+	//enterkey char code is 13
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle()
+});
